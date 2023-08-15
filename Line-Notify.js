baf4e9da208d21f7a7c2e85c4cf7d36b8a3d8531
Awirut  Armeen <34700025+Student554@users.noreply.github.com>
2023-08-15 15:12:38 +0700

    Add GetDescription

--- Line-Notify.js
+++ Line-Notify.js
@@ -1,42 +1,43 @@
 function lineNotify() {
-    var calendar = CalendarApp.getCalendarById("...");//ID Calendar
-    var token = "..."; // Line Token 
-    var url = "https://notify-api.line.me/api/notify";//Line Notify API
-  
-    var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
-    var event = calendar.getEventsForDay(today);
-    var msg = "";
-    if (event.length === 0) {
-      msg = "วันนี้ไม่มีกิจกรรม";
-    }
-    else {
-      msg += "📣📣วันนี้มีทั้งหมด " + String(event.length) + " กิจกรรม ได้แก่\n\n";
-      msg += sendMessage(event);
-    }
-    var jsonData = {
-      message: msg
-    }
-    var options =
-    {
-      "method": "post",
-      "contentType": "application/x-www-form-urlencoded",
-      "payload": jsonData,
-      "headers": { "Authorization": "Bearer " + token }
-    };
-    var res = UrlFetchApp.fetch(url, options);
+  var calendar = CalendarApp.getCalendarById("...");//ID Calendar
+  var token = "..."; // Line Token 
+  var url = "https://notify-api.line.me/api/notify";//Line Notify API
+
+  var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+  var event = calendar.getEventsForDay(today);
+  var msg = "";
+  if (event.length === 0) {
+    msg = "วันนี้ไม่มีกิจกรรม";
   }
-  
-  function sendMessage(events) {
-    var msg = "";
-    events.forEach(function (event, index) {
-      var title = event.getTitle();
-      var start = event.getStartTime().getHours() + ":" + ("0" + event.getStartTime().getMinutes()).slice(-2);
-      var end = event.getEndTime().getHours() + ":" + ("0" + event.getEndTime().getMinutes()).slice(-2);
-      if (event.isAllDayEvent()) {
-        msg += String(index + 1) + ") " + "เวลา : " + " ทั้งวัน" + "\n เรื่อง : " + title + "\n\n";
-        return;
-      }
-      msg += String(index + 1) + ") " + "เวลา : " + start + " - " + end + " น." + "\nเรื่อง : " + title + "\n\n";
-    });
-    return msg;
+  else {
+    msg += "📣📣วันนี้มีทั้งหมด " + String(event.length) + " กิจกรรม ได้แก่\n\n";
+    msg += sendMessage(event);
   }
+  var jsonData = {
+    message: msg
+  }
+  var options =
+  {
+    "method": "post",
+    "contentType": "application/x-www-form-urlencoded",
+    "payload": jsonData,
+    "headers": { "Authorization": "Bearer " + token }
+  };
+  var res = UrlFetchApp.fetch(url, options);
+}
+
+function sendMessage(events) {
+  var msg = "";
+  events.forEach(function (event, index) {
+    var title = event.getTitle();
+    var des = event.getDescription();
+    var start = event.getStartTime().getHours() + ":" + ("0" + event.getStartTime().getMinutes()).slice(-2);
+    var end = event.getEndTime().getHours() + ":" + ("0" + event.getEndTime().getMinutes()).slice(-2);
+    if (event.isAllDayEvent()) {
+      msg += String(index + 1) + ") " + "เวลา : " + " ทั้งวัน" + "\n 🔥เรื่อง : " + title + "\n👉 รายละเอียด : " + des + "\n\n";
+      return;
+    }
+    msg += String(index + 1) + ") " + "เวลา : " + start + " - " + end + " น." + "\n🔥เรื่อง : " + title + "\n👉 รายละเอียด : " + des + "\n\n";
+  });
+  return msg;
+}
